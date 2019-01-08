2fb0e765e0edd0672289bb8049616ec9ab9e5c60
PRÉLOT Vincent <vpre294@darva.intra>
2019-01-08 16:53:16 +0100

    rationalisation

---
+++ docker/mongodb/3.6_with_sharding/scripts/shard.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+su - mongodb -c "mongod --shardsvr --replSet $(hostname) --port 27017 --bind_ip_all --fork --syslog --pidfilepath /tmp/mongodb.pid"
+echo "init shard $(hostname)"
+mongo <<EOF
+rs.initiate({
+  _id: "${hostname}",
+  members: [{ _id: 0, host: "$(hostname):27017" }]
+});
+sleep(2000)
+EOF
+kill $(cat /tmp/mongodb.pid) > /dev/null
+while ps -p$(cat /tmp/mongodb.pid) > /dev/null
+do
+  sleep 5
+  ps -fp$(cat /tmp/mongodb.pid)
+done
+exec "$@"
